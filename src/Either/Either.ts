ad7043ddc2d6698c9bf14defb3c3d0163f4992a9
Ben <ben399399@naver.com>
2022-05-30 01:06:54 +0900

    feat: wip on Either

--- src/Either/Either.ts
+++ src/Either/Either.ts
@@ -1,1 +1,17 @@
 export type Either<A,B> = A|B
+
+export type Left<A> = {
+  _: 'Left',
+  value:A
+}
+
+export type Right<A> = {
+  _: 'Right',
+  value:A
+}
+
+function left()
+
+export const Either = {
+  of
+}
